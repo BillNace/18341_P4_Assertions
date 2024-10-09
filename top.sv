f89313ceff5b124e5335e953ae4ed0f349a033dd
Bill Nace <wnace@cmu.edu>
2024-10-08 22:43:18 -0400

    Updated for F24

--- top.sv
+++ top.sv
@@ -8,14 +8,14 @@
 
   typedef enum logic [3:0] {START    = 4'h1,
                             ENTER    = 4'h2,
-                            ARITH_OP = 4'h4, 
-                            DONE     = 4'h8} oper_t; 
-                            
-  typedef struct packed { // what appears at the data input 
+                            ARITH_OP = 4'h4,
+                            DONE     = 4'h8} oper_t;
+
+  typedef struct packed { // what appears at the data input
     oper_t       op;
-    logic [15:0] payload; 
-    } keyIn_t; 
-    
+    logic [15:0] payload;
+    } keyIn_t;
+
 `endif
 
 
@@ -33,24 +33,24 @@ module top();
 
     //outputs from calculator
     logic [15:0]  result;
-    logic         stackOverflow, unexpectedDone, protocolError, dataOverflow, 
+    logic         stackOverflow, unexpectedDone, protocolError, dataOverflow,
                 correct, finished;
 
     logic [7:0][15:0] stackOut;
-  
+
     //calculator instantiation
     TA_calc  brokenCalc(.*);
 
     //the system clock
     initial begin
-        clock = 0;
+        clock = 1'b0;
         forever #5 clock = ~clock;
     end
 
     //this task should contain your testbench
     task runTestbench(input int phase);
       begin
-        /* * * * * * * * * * * * 
+        /* * * * * * * * * * * *
          * YOUR TESTBENCH HERE *
          * * * * * * * * * * * */
         //test for functional "correct", "finished", and ADD
@@ -73,8 +73,8 @@ module top();
           else $display("result is %d", result);
       end
     endtask
-      
-    /* * * * * * * * * * * * * 
+
+    /* * * * * * * * * * * * *
      * YOUR ASSERTIONS HERE  *
      * * * * * * * * * * * * */
 
